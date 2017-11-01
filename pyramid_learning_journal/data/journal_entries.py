86ef1c376e079867d0b65d0da1e045a11ab88608
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-31 18:15:40 -0700

    creating pages using jinja

---
+++ pyramid_learning_journal/data/journal_entries.py
@@ -0,0 +1,18 @@
+JOURNAL_ENTRIES = [
+    {
+        'id': 1,
+        'title': 'Pyramid and CookieCutter',
+        'date': 'October 30, 2017',
+        'body': "So today we learned about pyramid, which was chaos at first,\
+          but after I finally understood how it all worked together it got better.\
+          I wouldn't say easier, because I still feel like everything could\
+          break at any given second and I know it's going to get worse the\
+          more we add, but I definitely understand what's going on now.\
+          Also, we learned cookiecutter, which is the best thing ever.\
+          Although it brings more potential moments of self destruction,\
+          it helps you make sure nothing was forgotten and starts your project off right.\
+          Even if it only stays that way until I get my hands on it."
+    },
+    {'id': 2, 'title': 'Title 2', 'date': 'date', 'body': 'WTF'},
+    {'id': 3, 'title': 'Title 3', 'date': 'date', 'body': 'HALP'}
+]
