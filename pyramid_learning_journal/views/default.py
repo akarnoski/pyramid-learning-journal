fc07f432cb85c849169afb3238885a657d48e8f1
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-04 16:05:29 -0700

    added tests and fixed files

--- pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/views/default.py
@@ -8,20 +8,24 @@ TEMPLATES = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'templates')
 
 
 def list_view(request):
+    """Response that renders index.html view."""
     with open(os.path.join(TEMPLATES, 'index.html')) as file:
         return Response(file.read())
 
 
 def detail_view(request):
+    """Renders the detail page."""
     with open(os.path.join(DATA, 'oct30.html')) as file:
         return Response(file.read())
 
 
 def create_view(request):
+    """Renders the new entry page."""
     with open(os.path.join(TEMPLATES, 'form_page.html')) as file:
         return Response(file.read())
 
 
 def update_view(request):
+    """Renders the page for updating existing articles."""
     with open(os.path.join(TEMPLATES, 'edit_page.html')) as file:
         return Response(file.read())
