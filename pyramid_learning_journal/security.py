58952bee5a53bf1cadbfb74bd685e909fd494968
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-09 14:35:59 -0800

    fixed error with cookie token

--- pyramid_learning_journal/security.py
+++ pyramid_learning_journal/security.py
@@ -31,7 +31,7 @@ def includeme(config):
     config.set_authorization_policy(authz_policy)
     config.set_default_permission('view')
     config.set_root_factory(MyRoot)
-    session_secret = os.environ['SESSION_SECRET']
+    session_secret = os.environ.get('SESSION_SECRET', 'itsaseekrit')
     session_factory = SignedCookieSessionFactory(session_secret)
     config.set_session_factory(session_factory)
     config.set_default_csrf_options(require_csrf=True)
